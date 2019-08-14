c43a273c12d24cf156a6a7d707145fa863fce34a
Gordon zhang <guojun.blue@gmail.com>
2019-08-14 08:44:59 -0400

    Initial commit

---
+++ src/main/java/com/codehub/javaproxyplayground/JavaProxyPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.codehub.javaproxyplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProxyPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProxyPlaygroundApplication.class, args);
+	}
+
+}
